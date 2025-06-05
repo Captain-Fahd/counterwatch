dcaee38901f1e3bfa8ba386cb5dfd583c4df8cdf
Captain-Fahd <omarfahd06@gmail.com>
2025-06-05 14:56:10 +1000

    Implemented the populatedb file. Implemented the populateDB to create 3 tables, one for the heroes, role and for the counter && countered pairs.

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('node:path');
+//Middleware parser
+app.use(express.urlencoded({ extended: true }));
+//App routers
 const indexRouter = require('./routes/indexRouter');
 
+//App setup
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/', indexRouter);
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-
+//App listening
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
